d1bf9c98ddf09bc678b8c16e66672b81818e87cd
Matheus de Almeida <69125506+matheusmra@users.noreply.github.com>
2026-04-03 10:44:38 -0300

    adding repository

---
+++ assigments/a02/src/main/java/com/example/order/repository/ProdutoRepository.java
@@ -0,0 +1,9 @@
+package com.example.order.repository;
+
+import com.example.order.model.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+}
